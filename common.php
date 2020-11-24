de06fa0a969d092dd697dd52ca7b66577d533aa9
Diego Andrade <andrade.diego@outlook.com>
2020-11-24 09:51:10 -0500

    Added common.php

---
+++ common.php
@@ -0,0 +1,24 @@
+<?php 
+    // defines for windows
+    define('DBHOST', 'localhost');
+    define('DBDB', 'pizza_store');
+    define('DBUSER', 'root');
+    define('DBPW', '');
+
+    // defines for Linux
+    // define('DBHOST', 'localhost');
+    // define('DBDB', 'pizza_store');
+    // define('DBUSER', 'pizzauser');
+    // define('DBPW', 'pizza123');
+
+    function connectDB() {
+        $dsn = 'mysql:host='.DBHOST.';dbname='.DBDB.';charset=utf8';
+        try {
+            $db_conn = new PDO($dsn, DBUSER, DBPW);
+            return $db_conn;
+        } catch (PDOException $e) {
+            echo '<p>Error opening database: '.$e->getMessage().'</p>';
+            exit(1);
+        }
+    }
+?>
